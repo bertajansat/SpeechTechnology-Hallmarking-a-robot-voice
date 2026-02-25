77177c07e8d4a4b29671179dc7ae30006adf143e
root <root@BertaLenovo.localdomain>
2026-02-25 16:52:26 +0100

    Changing speed rate

--- post_processing.py
+++ post_processing.py
@@ -2,9 +2,31 @@ import numpy as np
 import librosa
 import librosa.display
 import soundfile as sf
-from pedalboard import Pedalboard, Distortion, Gain, Bitcrush, Clipping
+from pedalboard import Pedalboard, Distortion, Gain, Bitcrush, Clipping, MP3Compressor
 import math
+import pyrubberband
 
+
+def compression_artifacts(audio,sr): # --> Not very notable changes applied
+    compressor = MP3Compressor(vbr_quality=9.99)  # 0.0 = better quality, 9.99 = worse
+    
+    processed = compressor.process(
+        audio,
+        sample_rate=sr,
+        buffer_size=64
+    )
+    return processed.T
+
+def speaking_vel(audio, sr, v_rate, quality="low"): 
+    effected = np.copy(audio)
+    print(quality)
+    if quality=="high":
+        print("hola")
+        return pyrubberband.pyrb.time_stretch(effected, sr, rate=v_rate)
+    elif quality=="low":
+        return librosa.effects.time_stretch(effected,rate=v_rate)
+    else:
+        return None
 def random_n_steps(min_steps,max_steps,zero_prob):
     if np.random.rand() < zero_prob:
         return 0
@@ -14,9 +36,9 @@ def random_n_steps(min_steps,max_steps,zero_prob):
             if n != 0.0:
                 return n
 
-def pitch_switch(audio,sr,n_steps=2,bins_octave=6):
-    effected = np.copy(audio)
-    return librosa.effects.pitch_shift(effected,sr=sr,n_steps=n_steps,bins_per_octave=bins_octave,res_type='linear')
+def pitch_switch(audio,sr,n_steps=4,bins_octave=12):
+    effected = np.copy(audio)  # ratio = 2 ** (n_steps / bins_per_octave)
+    return librosa.effects.pitch_shift(effected,sr=int(sr),n_steps=n_steps,bins_per_octave=bins_octave,res_type='sinc_fastest')
 
 def pitch_contour(audio,sr,num_pitches=50,zero_prob=0.2, bins_octave=24):
     f0, voicing, voicing_probability = librosa.pyin(y=audio, sr=sr, fmin=50, fmax=300)
@@ -85,9 +107,12 @@ y_pitch_s = pitch_switch(y,sr)
 y_dist = distortion(y,sr)
 y_bitcrush = bitcrush(y,sr)
 y_clip = clipping(y,sr)
+y_vel = speaking_vel(y, sr, v_rate=0.65, quality="high")
 # Save generated audios
-sf.write("Generated/Post-processing/pitch_c_"+audio_name, y_pitch_c, sr)
-sf.write("Generated/Post-processing/pitch_s_"+audio_name, y_pitch_s, sr)
-sf.write("Generated/Post-processing/distorted_"+audio_name, y_dist, sr)
-sf.write("Generated/Post-processing/bitcrush_"+audio_name, y_bitcrush, sr)
-sf.write("Generated/Post-processing/clipping_"+audio_name, y_clip, sr)
+#sf.write("Generated/Post-processing/pitch_c_"+audio_name, y_pitch_c, sr)
+#sf.write("Generated/Post-processing/pitch_s_"+audio_name, y_pitch_s, sr)
+#sf.write("Generated/Post-processing/distorted_"+audio_name, y_dist, sr)
+#sf.write("Generated/Post-processing/bitcrush_"+audio_name, y_bitcrush, sr)
+#sf.write("Generated/Post-processing/clipping_"+audio_name, y_clip, sr)
+
+sf.write("Generated/Post-processing/vel_"+audio_name, y_vel, sr)
