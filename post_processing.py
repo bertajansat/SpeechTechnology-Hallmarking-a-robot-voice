614ea79acf194ca3f866bb20ad3741ad307b60fc
root <root@BertaLenovo.localdomain>
2026-02-26 14:55:02 +0100

    Robotic voices implemented + post_processing update

--- post_processing.py
+++ post_processing.py
@@ -7,6 +7,21 @@ import math
 import pyrubberband
 
 
+def pitch_time_shift(audio,sr, delay=0):
+   
+    depth = 0
+    mix = 0
+    n=len(y)
+    delay_samples = math.trunc(delay*sr)
+    t = np.arange(n) / sr
+    
+    y_high = pitch_switch(audio,sr,n_steps=2,bins_octave=12)
+    y_low = pitch_switch(audio,sr,n_steps=-2,bins_octave=12)
+
+    y_pitch = y_high + y_low
+    
+    return y_pitch
+
 def compression_artifacts(audio,sr): # --> Not very notable changes applied
     compressor = MP3Compressor(vbr_quality=9.99)  # 0.0 = better quality, 9.99 = worse
     
@@ -36,9 +51,9 @@ def random_n_steps(min_steps,max_steps,zero_prob):
             if n != 0.0:
                 return n
 
-def pitch_switch(audio,sr,n_steps=4,bins_octave=12):
+def pitch_switch(audio,sr,n_steps=4,bins_octave=12,type='soxr_lq'):
     effected = np.copy(audio)  # ratio = 2 ** (n_steps / bins_per_octave)
-    return librosa.effects.pitch_shift(effected,sr=int(sr),n_steps=n_steps,bins_per_octave=bins_octave,res_type='sinc_fastest')
+    return librosa.effects.pitch_shift(effected,sr=int(sr),n_steps=n_steps,bins_per_octave=bins_octave,res_type=type)
 
 def pitch_contour(audio,sr,num_pitches=50,zero_prob=0.2, bins_octave=24):
     f0, voicing, voicing_probability = librosa.pyin(y=audio, sr=sr, fmin=50, fmax=300)
@@ -97,9 +112,43 @@ def clipping(audio,sr, offset_db=7):  # Offset_db: 0 (no clipping), clipping inc
     effected = board(audio, sr)
     return effected.T 
 
+def flanger(y, sr, freq=3.5, m_0_ms=5, depth_ms=5, g=1, mix = 1): # Depth <= m_0!
+    # Flanger: y(n)=x(n)+g*x(n-M(n))
+
+    m_0=m_0_ms*sr/1000
+    depth = depth_ms*sr/1000
+    n=len(y)
+    t = np.arange(n) / sr
+    # M(n) = M0 + D * LFO(n) = M0 + D * sin(2*pi*f_LFO*n/fs)
+    lfo = np.sin(2 * np.pi * freq * t)
+    m = m_0 + depth*lfo
+
+    y_flanger = np.zeros(n)
+    for i in range(n):
+            delay = m[i]
+            idx = i - delay # Compute position where we must add the delay: x(n-M(n)) = x(idx)
+
+            if idx < 0:
+                delayed_sample = 0 # Set negative indices to 0
+            else:
+                # Lineal interpolation
+                i0 = int(np.floor(idx)) # Avoid decimal indeces
+                i1 = min(i0 + 1, n - 1) # Avoid surpassing vector size 
+                
+                # For non-whole numbers, compute average of nearby samples:
+                frac = idx - i0
+                delayed_sample = (1 - frac) * y[i0] + frac * y[i1]
+
+            y_flanger[i] = (1 - mix) * y[i] + mix * (y[i] + g * delayed_sample) # Mix to control how much the effect is applied
+
+    return y_flanger
+
+
+
+
 # Load audio
-audio_name="w3_cold_expresiveness.wav"
-y, sr = librosa.load("Generated/"+audio_name, sr=None)
+audio_name="stress_tired_woman.wav"
+y, sr = librosa.load("Generated_audios/"+audio_name, sr=None)
 
 # Shift formants
 y_pitch_c = pitch_contour(y,sr)
@@ -107,7 +156,10 @@ y_pitch_s = pitch_switch(y,sr)
 y_dist = distortion(y,sr)
 y_bitcrush = bitcrush(y,sr)
 y_clip = clipping(y,sr)
-y_vel = speaking_vel(y, sr, v_rate=0.65, quality="high")
+y_vel = speaking_vel(y, sr, v_rate=0.75, quality="low")
+
+y_shift_1 = flanger(y,sr)
+y_shift= pitch_switch(y_shift_1,sr,n_steps=1,bins_octave=12, type='soxr_hq')
 # Save generated audios
 #sf.write("Generated/Post-processing/pitch_c_"+audio_name, y_pitch_c, sr)
 #sf.write("Generated/Post-processing/pitch_s_"+audio_name, y_pitch_s, sr)
@@ -115,4 +167,4 @@ y_vel = speaking_vel(y, sr, v_rate=0.65, quality="high")
 #sf.write("Generated/Post-processing/bitcrush_"+audio_name, y_bitcrush, sr)
 #sf.write("Generated/Post-processing/clipping_"+audio_name, y_clip, sr)
 
-sf.write("Generated/Post-processing/vel_"+audio_name, y_vel, sr)
+sf.write("Generated_audios/Flanger + Pitch/pitch_1_soxr_hq_NO_"+audio_name, y_shift, sr)
